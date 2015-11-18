c78aac368f73b14207a9c1ab41458b494ade304d
leeseung <dwdw201@naver.com>
2015-11-19 08:26:46 +0900

    11/19

---
+++ diary/src/main/java/com/example/lee/myapplication/MainActivity.java
@@ -0,0 +1,104 @@
+package com.example.lee.myapplication;
+
+import android.app.Activity;
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Calendar;
+
+public class MainActivity extends Activity {
+    DatePicker dp;
+    EditText editDiary;
+    Button btnWrite;
+    String filename;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        dp = (DatePicker) findViewById(R.id.datePicker1);
+        editDiary = (EditText) findViewById(R.id.edtDiary);
+        btnWrite = (Button) findViewById(R.id.btnWrite);
+
+        Calendar cal = Calendar.getInstance();
+        int cYar = cal.get(Calendar.YEAR);
+        int cMonth = cal.get(Calendar.MONTH);
+        int cDay = cal.get(Calendar.DAY_OF_MONTH);
+
+        dp.init(cYar, cMonth, cDay, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                filename = Integer.toString(year) +"_"+Integer.toString(monthOfYear+1)+"_"
+                        +Integer.toString(dayOfMonth)+".txt";
+                String str = readDiary(filename);
+                editDiary.setText(str);
+                btnWrite.setEnabled(true);
+            }
+        });
+        btnWrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    FileOutputStream outFs = openFileOutput(filename, Context.MODE_WORLD_WRITEABLE);
+                    String str = editDiary.getText().toString();
+                    outFs.write(str.getBytes());
+                    outFs.close();
+                    Toast.makeText(getApplicationContext(),filename+"이 저장됨",0).show();
+                } catch (IOException e){
+
+                }
+            }
+        });
+    }
+    String readDiary(String fName){
+        String diaryStr = null;
+        FileInputStream inFs;
+        try{
+            inFs = openFileInput(fName);
+            byte[] txt = new byte[500];
+            inFs.read(txt);
+            inFs.close();
+            diaryStr = (new String(txt)).trim();
+            btnWrite.setText("수정하기");
+        }   catch (IOException e){
+            editDiary.setHint("일기 없음");
+            btnWrite.setText("새로 저장");
+        }
+        return diaryStr;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
