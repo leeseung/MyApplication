ca0d33cdee63d2dc87446c2ce8c85575d4078b59
leeseung <dwdw201@naver.com>
2015-11-29 14:11:09 +0900

    Poter, diaryApp

---
+++ poster/src/main/java/com/example/lee/myapplication/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.lee.myapplication;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Bundle;
+
+import android.view.View;
+
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("그리드뷰 영화포스터 ");
+
+
+        final GridView gv = (GridView) findViewById(R.id.gridView1);
+        MyGridAdapter gAdapter = new MyGridAdapter(this);
+        gv.setAdapter(gAdapter);
+
+
+    }
+
+    public class MyGridAdapter extends BaseAdapter {
+        Context context;
+
+        public MyGridAdapter(Context c) {
+            context = c;
+        }
+
+        @Override
+        public int getCount() {
+            return posterID.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        Integer[] posterID = {
+                R.drawable.mov01, R.drawable.mov02, R.drawable.mov03, R.drawable.mov04
+                , R.drawable.mov05, R.drawable.mov06, R.drawable.mov07, R.drawable.mov08,
+                R.drawable.mov09, R.drawable.mov10
+        };
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(100, 150));
+            imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+            imageView.setPadding(5, 5, 5, 5);
+
+            final int pos = position;
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    View dialogView = View.inflate(MainActivity.this, R.layout.dialog, null);
+                    AlertDialog.Builder dlg = new AlertDialog.Builder(MainActivity.this);
+                    ImageView ivPoster = (ImageView) dialogView.findViewById(R.id.ivPoster);
+                    ivPoster.setImageResource(posterID[pos]);
+                    switch (pos) {
+                        case 0:
+                            dlg.setTitle("하류인생");
+                            break;
+                        case 1:
+                            dlg.setTitle("올드보이");
+                            break;
+                        case 2:
+                            dlg.setTitle("I am");
+                            break;
+                        case 3:
+                            dlg.setTitle("특종");
+                            break;
+                        case 4:
+                            dlg.setTitle("쎄시봉");
+                            break;
+                        case 5:
+                            dlg.setTitle("Great Debaters");
+                            break;
+                        case 6:
+                            dlg.setTitle("트랜스머신 : 만테라");
+                            break;
+                        case 7:
+                            dlg.setTitle("악마를 보았다");
+                            break;
+                        case 8:
+                            dlg.setTitle("아마데우스");
+                            break;
+                        case 9:
+                            dlg.setTitle("트루맨");
+                            break;
+                    }
+                    dlg.setIcon(R.drawable.ic_launcher);
+                    dlg.setView(dialogView);
+                    dlg.setNegativeButton("닫기", null);
+                    dlg.show();
+                }
+            });
+
+            imageView.setImageResource(posterID[position]);
+            return imageView;
+        }
+    }
+
+}
+
