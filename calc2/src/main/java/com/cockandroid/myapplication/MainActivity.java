9d7432814d87bd0852e00492e2f85388d0bd8b0f
leeseung <dwdw201@naver.com>
2015-12-10 15:04:06 +0900

    calc2

--- calc2/src/main/java/com/cockandroid/myapplication/MainActivity.java
+++ calc2/src/main/java/com/cockandroid/myapplication/MainActivity.java
@@ -25,7 +25,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.table_layout);
 
         int i;
         edit1 = (EditText) findViewById(R.id.edit1);
